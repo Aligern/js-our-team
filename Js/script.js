96a8b09daec073ff7c111368a3fa6db2c85926b0
Aligern <sasso.luca93@gmail.com>
2024-03-21 14:22:53 +0100

    first commit

---
+++ Js/script.js
@@ -0,0 +1,34 @@
+
+// this is our team members array:
+const team =[ {
+    name: 'Wayne Barnett',
+    role:   'Founder & CEO',
+    avatar: ' wayne-barnett-founder-ceo.jpg'
+},
+{
+    name: 'Angela Caroll',
+    role:   'Chief Editor',
+    avatar: 'angela-caroll-chief-editor.jpg'
+}, 
+{
+    name: 'Walter Gordon',
+    role:   'Office Manager',
+    avatar: ' wayne-barnett-founder-ceo.jpg'
+}, 
+{
+    name: 'Angela Lopez',
+    role:   'Social Media Manager',
+    avatar: ' wayne-barnett-founder-ceo.jpg'
+}, 
+{
+    name: 'Scott Estrada',
+    role:   'Developer',
+    avatar: ' wayne-barnett-founder-ceo.jpg'
+}, 
+{
+    name: 'Barbara Ramos',
+    role:   'Graphic Designer',
+    avatar: ' wayne-barnett-founder-ceo.jpg'
+}, 
+];
+
