c7a4af269460ab1c921f52d3b2d2bb3a1eca3cfa
Aligern <sasso.luca93@gmail.com>
2024-03-21 15:14:14 +0100

    almost there

--- Js/script.js
+++ Js/script.js
@@ -31,6 +31,25 @@ const team =[ {
 }, 
 ];
 
+// here we print on console name, role and avatar for each team member:
 for (let key of team) {
     console.log(key.name, key.role, key.avatar);
+};
+
+createList();
+
+function createList(){
+    const elContainer = document.getElementById('contenitore');
+    const elUl = document.createElement('ul');
+    const elLi = document.createElement('li');
+    elContainer.appendChild(elUl);
+    for (let i = 0; i < team.length; i++) {
+        const elLi = document.createElement('li');
+        elLi.innerHTML =`
+        Name: ${team[i].name},
+        Role: ${team[i].role},
+        Avatar ${team[i].avatar}
+    `
+    elUl.appendChild(elLi);
+    }
 };
